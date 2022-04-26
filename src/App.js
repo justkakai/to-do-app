aba4a7d776ea8f79a2ba69be581f5eaaf0f381a3
Kakai <kenkai24@yahoo.com>
2022-04-26 17:10:40 +0200

    added custom scrollbar

--- src/App.js
+++ src/App.js
@@ -12,16 +12,6 @@ function App() {
     localStorage.setItem('theArray', JSON.stringify(theArray));
   }, [theArray])
 
-  /*
-  useEffect(() => {
-    console.log(theArray.indexOf(itemToRemove));
-    theArray.splice(theArray.indexOf(itemToRemove), 1);
-    setArrayItem(theArray);
-    console.log('itemToRemove', itemToRemove);
-    console.log(theArray);
-  }, [itemToRemove])
-  */
-
   const removeItem = function (item) {
     theArray.splice(theArray.indexOf(item), 1);
     setArrayItem([...theArray]);
@@ -29,17 +19,17 @@ function App() {
 
   return (
     <div className="App">
-      <h1>You have {theArray.length} items on your to-do list</h1>
+      <h1>you have {theArray.length} items on your to-do list</h1>
       <div className='input-container'>
         <input type="text" onChange={(e) => setInputToAdd(e.target.value)} value={inputToAdd} />
-        <button onClick={() => setArrayItem([...theArray, inputToAdd])}>Add item</button>
+        <button onClick={() => setArrayItem([...theArray, inputToAdd])}>add item</button>
       </div>
       <ul className='listing'>
         {theArray.map(item => {
           return (
             <div className='toDoItem'>
               <li className="arrayItems">{item}</li>
-              <button onClick={() => {removeItem(item)}}>remove</button>
+              <button onClick={() => {removeItem(item)}}>✓</button>
             </div>
           )
         })}
